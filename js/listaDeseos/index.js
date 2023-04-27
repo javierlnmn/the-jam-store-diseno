e76a7f6ed78616cdb7bf5ba4463bfdce17a6cc4a
javierlnmn <jlm03123@gmail.com>
2023-04-27 11:29:41 +0200

    Avances filtros

--- js/listaDeseos/index.js
+++ js/listaDeseos/index.js
@@ -6,14 +6,6 @@ botonVerFiltros.on('click', function () {
     seccionFiltros.show('fast');
 });
 
-
 botonCerrarFiltros.on('click', function () {
     seccionFiltros.hide('fast');
-});
-
-$(document).on('keydown', function (event) {
-    if (event.keyCode == 27) seccionFiltros.hide('fast', function() {
-        seccionFiltros.css('overflow', 'hidden')
-    });
-    $('body').css('overflow', 'auto');
 });
