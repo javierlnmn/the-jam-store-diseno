95be0a4687dd5ad5d15355c828dac778ea62a928
javierlnmn <jlm03123@gmail.com>
2023-05-09 20:18:53 +0200

    Carrito incializado

--- js/script.js
+++ js/script.js
@@ -132,3 +132,19 @@ jQuery('.numero-articulos').each(function () {
     });
 
 });
+
+// Desplegar seccion de ajustes
+
+let botonVerAjustesBusqueda = $('#boton-ver-ajustes-busqueda');
+let botonCerrarAjustesBusqueda = $('#boton-cerrar-ajustes-busqueda');
+let seccionAjustesBusqueda = $('.seccion-ajustes-busqueda');
+
+botonVerAjustesBusqueda.on('click', function () {
+    seccionAjustesBusqueda.show('fast', function() {
+        seccionAjustesBusqueda.css('display', 'block');
+    });
+});
+
+botonCerrarAjustesBusqueda.on('click', function () {
+    seccionAjustesBusqueda.hide('fast');
+});
