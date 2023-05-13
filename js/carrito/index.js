0fd29e8de99d6b0e32a9ecacc92982196b3ae5b3
javierlnmn <jlm03123@gmail.com>
2023-05-13 12:31:30 +0200

    Avances carrito compra

--- js/carrito/index.js
+++ js/carrito/index.js
@@ -16,3 +16,22 @@ $(document).on('keydown', function (event) {
     if (event.keyCode == 27) /* 27 == ESC */ modalVaciarCarrito.hide();
     $('body').css('overflow', 'auto');
 });
+
+// Quitar sombra del banner del precio
+
+let pieCarrito = $('.pie-carrito');
+let distanciaAnterior = pieCarrito.offset();
+
+$(window).on('scroll', function () {
+    let distancia = pieCarrito.offset().top;
+
+    console.log(distancia);
+    console.log(distanciaAnterior);
+    if (distancia === distanciaAnterior) {
+        pieCarrito.addClass('pie-carrito_sin-sombra');
+    } else {
+        pieCarrito.removeClass('pie-carrito_sin-sombra');
+    }
+
+    distanciaAnterior = distancia;
+});
